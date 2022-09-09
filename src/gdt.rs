ed9aeb31fed2c8430634eacea3713e4354cfc1b3
yinggu <ying.gu@hiraku.cloud>
2022-09-09 17:29:28 +0000

    Create new GDT with code segment and TSS. Active GDT by reloading code segment register and TSS selector

--- src/gdt.rs
+++ src/gdt.rs
@@ -2,6 +2,9 @@ use x86_64::VirtAddr;
 //TaskStateSegment:Find the kernel level stack if the interrupt arrives in kernel mode
 use x86_64::structures::tss::TaskStateSegment;
 use lazy_static::lazy_static;
+use x86_64::structures::gdt::{GlobalDescriptorTable,Descriptor};
+use x86_64::structures::gdt::SegmentSelector;
+
 
 pub const DOUBLE_FAULT_IST_INDEX:u16 = 0;
 
@@ -21,4 +24,37 @@ lazy_static!{
         };
         tss
     };
+}
+
+    //create a GDT which include the static tts
+lazy_static!{
+    static ref GDT:(GlobalDescriptorTable,Selectors) = {
+        let mut gdt = GlobalDescriptorTable::new();
+        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
+        (gdt, Selectors{code_selector,tss_selector})
+
+
+        };
+    }
+
+struct Selectors{
+    code_selector:SegmentSelector,
+    tss_selector:SegmentSelector,
+
+}
+
+pub fn init(){
+    use x86_64::instructions::tables::load_tss;
+    use x86_64::instructions::segmentation::{CS,Segment};
+
+    
+    GDT.0.load();
+    unsafe{
+        //load code segment register
+        //load TSS, now CPU has access to a valid interrupt stack table
+        CS::set_reg(GDT.1.code_selector);
+        load_tss(GDT.1.tss_selector);
+    }
+
 }
