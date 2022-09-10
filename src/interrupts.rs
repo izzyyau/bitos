233a5db1cce1d4f109c923000b4ecafe7eb91538
yinggu <ying.gu@hiraku.cloud>
2022-09-10 15:50:53 +0000

    add keyboard interrupt handler function to Interrupt Descriptor Table in order to implement Keyboard Input

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -12,6 +12,7 @@ use spin;
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer  = PIC_1_OFFSET,
+    Keyboard,
 }
 
 impl InterruptIndex{
@@ -44,6 +45,7 @@ lazy_static!{
             idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         } 
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -73,6 +75,14 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame:InterruptStackFra
     println!(".");
     unsafe{
         PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
+
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame:InterruptStackFrame){
+    println!("k");
+    //notify PIC the interrupt handler for current interrupt has done its job
+    unsafe{
+        PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
 }
 
