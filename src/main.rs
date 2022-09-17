323cbb0c461a4b30c72873a24f2ab63c294e896e
yinggu <ying.gu@hiraku.cloud>
2022-09-17 00:56:20 +0000

    Access Level 4 page table, and print non-empty entries

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,24 @@ use bitos::println;
 
 
 
-
+use bootloader::{BootInfo,entry_point};
+entry_point!(kernel_main);
 #[no_mangle]
-pub extern "C" fn _start() ->! {
+//defind Rust function as entry point
+fn kernel_main(boot_info : &'static BootInfo) ->! {
+    use x86_64::VirtAddr;
+    use bitos::memory::active_level_4_table;
     println!("Hello World{}","!");
     //initalize the idt 
     bitos::init();
-    //temporaily pause a prgram when the breakpoint instruction int3 is executed.
-    //x86_64::instructions::interrupts::int3();
 
-    let ptr = 0x2031b2 as *mut u32;
-    //read 
-    unsafe{let x = *ptr;}
-    println!("read worker");
-
-    //write
-    unsafe{*ptr = 42;}
-    println!("write worked");
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let l4_table = unsafe{active_level_4_table(phys_mem_offset)};
+    for(i,entry) in l4_table.iter().enumerate(){
+        if!entry.is_unused(){
+            println!("L4 Entry {} : {:?}", i, entry);
+        }
+    }
 
 
     //cfg is a special conditional compilation, compile code based on flag
