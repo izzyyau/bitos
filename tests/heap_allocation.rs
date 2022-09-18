b82ee0da9f8e957135d22a0b900f6f459475d16b
yinggu <ying.gu@hiraku.cloud>
2022-09-18 18:39:17 +0000

    Test heap allocator

---
+++ tests/heap_allocation.rs
@@ -0,0 +1,71 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(bitos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate alloc;
+use alloc::{boxed::Box, vec::Vec};
+use bitos::allocator::HEAP_SIZE;
+use bootloader::{entry_point,BootInfo};
+use core::panic::PanicInfo;
+use bitos::{serial_print,serial_println};
+
+entry_point!(main);
+
+
+fn main(boot_info: &'static BootInfo) -> ! {
+    use bitos::allocator;
+    use bitos::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
+
+    bitos::init();
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
+
+    test_main();
+    loop {}
+}
+
+//simple box allocation checks
+#[test_case]
+fn simple_allocation() {
+    let heap_value_1 = Box::new(41);
+    let heap_value_2 = Box::new(13);
+    assert_eq!(*heap_value_1, 41);
+    assert_eq!(*heap_value_2, 13);
+}
+
+//test large allocations and multiple allocations
+#[test_case]
+fn large_vec() {
+    serial_print!("large_vec... ");
+    let n = 1000;
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(i);
+    }
+    assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
+    serial_println!("[ok]");
+}
+
+//create ten thousand allocations after each other and test the allocator reuses freed 
+//memory.
+#[test_case]
+fn many_boxes() {
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+}
+
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    bitos::test_panic_handler(info)
+}
