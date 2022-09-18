a1d6c3e5bec97bf70019a417c837dca0fdeed0df
yinggu <ying.gu@hiraku.cloud>
2022-09-18 14:44:53 +0000

    Call init_heap method

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,41 @@
 
 use core::panic::PanicInfo;
 use bitos::println;
-
-
-
+use bitos::memory;
+use bitos::allocator;
 use bootloader::{BootInfo,entry_point};
+use x86_64::structures::paging::frame;
+use x86_64::structures::paging::{Translate,Page};
+extern crate alloc;
+use alloc::boxed::Box;
+
 entry_point!(kernel_main);
 #[no_mangle]
 //defind Rust function as entry point
 fn kernel_main(boot_info : &'static BootInfo) ->! {
     use x86_64::VirtAddr;
-    use bitos::memory::active_level_4_table;
+    use x86_64::structures::paging::mapper::MapperAllSizes;
     println!("Hello World{}","!");
     //initalize the idt 
     bitos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe{active_level_4_table(phys_mem_offset)};
-    for(i,entry) in l4_table.iter().enumerate(){
-        if!entry.is_unused(){
-            println!("L4 Entry {} : {:?}", i, entry);
-        }
-    }
 
+    use bitos::memory::BootInfoFrameAllocator;
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe{BootInfoFrameAllocator::init(&boot_info.memory_map)} ;
+    let x = Box::new(41);
+    // // map an unused page
+    // let page = Page::containing_address(VirtAddr::new(0));
+    // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    // // write the string `New!` to the screen through the new mapping
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // //The “New!” on the screen is caused by our write to page 0, 
+    // //which means that we successfully created a new mapping in the page tables.
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+    .expect("heap initialization failed");
     //cfg is a special conditional compilation, compile code based on flag
     #[cfg(test)]
     test_main();
