cdc1fdf0556739ae979f127fc68c990dc5264edb
yinggu <ying.gu@hiraku.cloud>
2022-09-20 02:54:02 +0000

    add poll method to allow the executor to poll the stored future

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -1,6 +1,6 @@
 use core::{future::Future, pin::Pin};
 use alloc::boxed::Box;
-
+use core::task::{Context, Poll};
 pub struct Task {
     future: Pin<Box<dyn Future<Output = ()>>>,
 }
@@ -8,8 +8,14 @@ pub struct Task {
 impl Task {
     pub fn new(future: impl Future<Output = ()> + 'static ) -> Task{
         Task { 
+            //pin the future in memory using Box::pin 
             future:Box::pin(future),
          }
+    }
+
+    //add poll method to allow the executor to poll the stored future
+    fn poll(&mut self, context:&mut Context) -> Poll<()>{
+        self.future.as_mut().poll(context)
     }
 }
 
