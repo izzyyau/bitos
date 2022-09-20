4e0d3b10caa680f699870e485b7b05c92adcbb53
yinggu <ying.gu@hiraku.cloud>
2022-09-20 03:58:19 +0000

    Implement Scancode_Queue and Stream trait for reading the scancodes from the queue in an asynchronous way.

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -1,6 +1,11 @@
 use conquer_once::spin::OnceCell;
 use crossbeam_queue::ArrayQueue;
 use crate::println;
+
+use core::{pin::Pin, task::{Poll, Context}};
+use futures_util::stream::Stream;
+
+
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
 
 /// Called by the keyboard interrupt handler
@@ -13,5 +18,30 @@ pub(crate) fn add_scancode(scancode: u8) {
         }
     } else {
         println!("WARNING: scancode queue uninitialized");
+    }
+}
+
+pub struct ScancodeStream {
+    _private: (),
+}
+
+impl ScancodeStream {
+    pub fn new() -> Self {
+        SCANCODE_QUEUE.try_init_once(|| ArrayQueue::new(100))
+            .expect("ScancodeStream::new should only be called once");
+        ScancodeStream { _private: () }
+    }
+}
+
+
+impl Stream for ScancodeStream {
+    type Item = u8;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
+        let queue = SCANCODE_QUEUE.try_get().expect("not initialized");
+        match queue.pop() {
+            Ok(scancode) => Poll::Ready(Some(scancode)),
+            Err(crossbeam_queue::PopError) => Poll::Pending,
+        }
     }
 }
