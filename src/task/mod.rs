f1e084d8994b0cbb14d5f4279177992d51c61931
yinggu <ying.gu@hiraku.cloud>
2022-09-20 14:48:29 +0000

    Add TaskId struct to Task struct in order to implement Waker Support for Executor

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -1,8 +1,13 @@
 use core::{future::Future, pin::Pin};
 use alloc::boxed::Box;
 use core::task::{Context, Poll};
+use core::sync::atomic::{AtomicU64, Ordering};
+pub mod simple_executor;
+pub mod keyboard;
+pub mod executor;
 pub struct Task {
     future: Pin<Box<dyn Future<Output = ()>>>,
+    id: TaskId, 
 }
 
 impl Task {
@@ -10,6 +15,8 @@ impl Task {
         Task { 
             //pin the future in memory using Box::pin 
             future:Box::pin(future),
+            //The new id field makes it possible to uniquely name a task, which is required for waking a specific task.
+            id: TaskId::new(),
          }
     }
 
@@ -19,3 +26,21 @@ impl Task {
     }
 }
 
+
+//The first step in creating an executor with proper support for waker notifications 
+//is to give each task a unique ID. 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct TaskId(u64);
+
+
+impl TaskId {
+    fn new() -> Self {
+        //The function uses a static NEXT_ID variable of type AtomicU64 to ensure that each ID is assigned only once. 
+        //The fetch_add method atomically increases the value and returns the previous value in one atomic operation.
+        //The Ordering parameter defines whether the compiler is allowed to reorder the fetch_add operation in the instructions stream.
+        //Since we only require that ID be unique, the Relaxed ordering with the weakest requirements
+        //is enough. 
+        static NEXT_ID: AtomicU64 = AtomicU64::new(0);
+        TaskId(NEXT_ID.fetch_add(1, Ordering::Relaxed))
+    }
+}
